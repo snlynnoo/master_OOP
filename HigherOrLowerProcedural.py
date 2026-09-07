a38bc4f9d0c5e44c138b0c4b3d8a721d3f9ddc0b
snlynnoo@gmail.com <snlynnoo@gmail.com>
2026-09-08 01:19:08 +0200

    gameappv.1

--- HigherOrLowerProcedural.py
+++ HigherOrLowerProcedural.py
@@ -33,3 +33,48 @@ for suit in SUIT_TUPLE:
         startingDeckList.append(cardDict)
     score = 50
 
+while True: # play multiple games
+    print()
+    gameDeckList = shuffle(startingDeckList)
+    currentCardDict = getCard(gameDeckList)
+    currentCardRank = currentCardDict['rank']
+    currentCardValue = currentCardDict['value']
+    currentCardSuite = currentCardDict['suit']
+    print('Starting card is:', currentCardRank + 'of ' + currentCardSuite)
+    print()
+
+    for cardNumber in range(0, NCARDS): # play one game of this many cards
+        answer = input('Will the next card be higher or lower than the'
+                       + currentCardRank + 'of '
+                       + currentCardSuite + '? (enter h or l): ')
+        answer = answer.casefold() # force lowercase
+        nextCardDict = getCard(gameDeckList)
+        nextCardRank = nextCardDict['rank']
+        nextCardValue = nextCardDict['value']
+        nextCardSuite = nextCardDict['suit']
+        print('Next card is:', nextCardRank + ' of ' + nextCardSuite)
+
+        if answer == 'h':
+            if nextCardValue > currentCardValue:
+                print('You got it right, it was higher')
+            else:
+                print('Sorry, it was not higher')
+                score = score - 15
+        elif answer == 'l':
+            if nextCardValue < currentCardValue:
+                score = score + 20
+                print('You got it right, it was lower')
+            else:
+                print('Sorry, it was not lower')
+                score = score - 15
+
+        print('Your score is:', score)
+        print()
+        currentCardRank = nextCardRank
+        currentCardValue = nextCardValue # don't need current suit
+
+        goAgain = input('To play again, press Enter, or "q" to quit:')
+        if goAgain.lower() == 'q':
+            break
+    print('OK, bye')
+
